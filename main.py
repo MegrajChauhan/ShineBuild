8e1471ea84c78f24f0ffee0cffe924439bd197c7
Megraj Chauhan <megrajchauhan123@gmail.com>
2024-08-29 21:10:13 +0545

    The humble beginning

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+It has been a long time since I last used Python for a project so this Build System will be written in Python
+SB = Shine Build
+I just randomly chose the name based on the lyrics I heard just now.
+Song- Belong Together by Mark Ambor
+"""
